d7111cb1ac4ca9a3a1d69b52bd893f545e7d89d0
chandra-98a <chandrakalaa2022@gmail.com>
2022-09-15 07:36:23 +0530

    "addition-program" folder has been committed.

---
+++ addition-program/src/main/java/com/hellojava/Addition.java
@@ -0,0 +1,8 @@
+package com.hellojava;
+
+public class Addition {
+	public int add(int a,int b) {
+		return a+b;
+	}
+
+}
---
+++ addition-program/src/test/java/com/hellojava/AdditionTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.hellojava;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ca5
+ *
+ */
+class AdditionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
