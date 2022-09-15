772fab3ac084c8c2cfa8b7625f2d2b4a9d58ee9a
chandra-98a <chandrakalaa2022@gmail.com>
2022-09-15 17:07:03 +0530

    "creating the testclass"

---
+++ addition-programming/src/test/java/com/junitlearnig/Addition1Test.java
@@ -0,0 +1,14 @@
+package com.junitlearnig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Addition1Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
