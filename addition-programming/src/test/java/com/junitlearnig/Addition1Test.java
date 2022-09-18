0b345ad736c1e123a707ad61fcd0c80a1b776cb8
chandra-98a <chandrakalaa2022@gmail.com>
2022-09-18 13:53:42 +0530

    "using tags,TestInfo,TestReporter"

--- addition-programming/src/test/java/com/junitlearnig/Addition1Test.java
+++ addition-programming/src/test/java/com/junitlearnig/Addition1Test.java
@@ -5,13 +5,19 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -23,9 +29,12 @@ class  Addition1Test{
 		System.out.println("This needs to run before all");
 	}
 	
-	
+	TestInfo testInfo;
+	TestReporter testReporter;
 	@BeforeEach
-	void init() {
+	void init(TestInfo testInfo,TestReporter testReporter) {
+		this.testReporter= testReporter;
+		this.testInfo=testInfo;
 		ad=new Addition1();
 	}
 
@@ -47,7 +56,7 @@ class  Addition1Test{
 	@DisplayName("Positive")
 	void testAddPositive() {
 		int expected=2;
-		int actual=ad.add(11, 1);
+		int actual=ad.add(1, 1);
 		assertEquals(expected,actual,"should return positive result"+expected+"but return"+actual);
 	}
 	@Test
@@ -56,7 +65,7 @@ class  Addition1Test{
 		assertEquals(-2,ad.add(-1, -1),"should return negative result");
 	}
 }
-	
+	@Tag("Math")
 	@Test
 	@DisplayName("testing multiply method")
 	void testMultiply() {
@@ -66,12 +75,14 @@ class  Addition1Test{
 				()->assertEquals(0,ad.multiply(1, 0)),
 				()->assertEquals(1,ad.multiply(1, 1))
 				);
+		System.out.println("running"+testInfo.getDisplayName()+"with tags"+testInfo.getTags());
 	}
 	
 	
 	
 	
 	@Test
+	@Tag("Math")
 	@DisplayName("testing divide method")
 	void testDivide() {
 		boolean isServerUp=true;
@@ -80,10 +91,12 @@ class  Addition1Test{
 	}
 	
 	@Test
+	@Tag("Circle")
+	@RepeatedTest(3)
 	@DisplayName("computing circle area method")
 	void testcomputeCircleArea() {
 		
-		assertEquals(314.1592653589793,ad.computeCircleArea(10),"should return right circle area");
+assertEquals(314.1592653589793,ad.computeCircleArea(10),"should return right circle area");
 	}
 	@AfterEach
 	void cleanUp()
